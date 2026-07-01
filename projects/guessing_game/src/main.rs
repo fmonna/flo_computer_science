4414ceabcdc17971f3056ac71c68dce5603df402
Florence Monna <florence.monna@gmail.com>
2026-07-01 11:27:38 +0200

    tiny repo cleanup

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -7,8 +7,6 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    println!("The secret_number is {secret_number}");
-
     println!("Please input your guess.");
 
     let mut guess = String::new();
